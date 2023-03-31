47548f2d45f0a3510d08c5c85609b8d77efb8439
Juan Gomes <juangsantanna@gmail.com>
2023-03-31 00:09:54 -0300

    primeiro commit

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O titulo do filme é obrigatório")]
+    public string titulo { get; set; }
+
+    [Required (ErrorMessage = "O tempo de duração do filme é obrigatório")]
+    [MinLength(5, ErrorMessage = "O tempo é menor do que o permitido - 5 minutos")]
+    [MaxLength(400, ErrorMessage = "O tempo exece o limite permitido: 500 minutos")]
+    public int tempoDeDuracao { get; set; }
+
+    [Required (ErrorMessage = "O genero do filme é obrigatório")]
+    [Range(5, 600, ErrorMessage ="A duração do filme deve ser entre 5 e 600 minutos")]
+    public string genero { get; set; }
+
+    [Required (ErrorMessage = "O país de origem é obrigatório")]
+    public string pais { get; set; }
+
+    [Required (ErrorMessage = "O nome do diretor é obrigatório")]
+    public string diretor { get; set; }
+}
